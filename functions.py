12339082605e2d9d7ac9d9aa72706a1c2427c190
Genius1512 <silvan.genius@gmail.com>
2021-10-25 09:19:13 -0700

    added comments for the functions script

--- functions.py
+++ functions.py
@@ -1,15 +1,18 @@
+# These are functions that are used by the server and the client. They handle
+# the conversion from dictionnarys to strings, so the networking server can convert it into bytes, and
+# the conversion from strings to the board.
+
 import os
 import sys
 from pyfiglet import figlet_format as banner
 
-
+# clear the console
 def clear():
     cmd = "clear"
     if "win" in sys.platform:
         cmd = "cls"
     os.system(cmd)
-
-
+# print out the board
 def get_board():
     board = {}
     for letter in ["a", "b", "c"]:
@@ -18,6 +21,10 @@ def get_board():
             board[letter][number] = "-"
 
     return board
+# convert the board into a string: x-oxxoox- =
+#                                   x-o
+#                                   xxo
+#                                   ox-
 
 def board_to_str(inp: dict):
     out = ""
@@ -26,7 +33,7 @@ def board_to_str(inp: dict):
             out += inp[letter][num]
 
     return out
-
+# convert the string back to a board
 def str_to_board(inp: str):
     out = {}
     inp = list(inp)
@@ -38,7 +45,10 @@ def str_to_board(inp: str):
             ind += 1
 
     return out
-
+# convert a dict to a string: reason/end|content/x-oxxoox = 
+#                                                           {"reason": "end",
+#                                                            "content": "x-oxxoox"
+#                                                           }
 def dict_to_str(inp: dict):
     out = ""
     for obj in inp:
@@ -46,7 +56,7 @@ def dict_to_str(inp: dict):
     out = out[:-1]
 
     return out
-
+# convert string to dict
 def str_to_dict(inp: str):
     out = {}
     inp = inp.split("|")
@@ -54,7 +64,7 @@ def str_to_dict(inp: str):
         out[obj.split("/")[0]] = obj.split("/")[1]
 
     return out
-
+# prints out the board
 def print_board(board):
     print(banner(f"""         1  2  3
 a {board["a"]["1"]} {board["a"]["2"]} {board["a"]["3"]}
